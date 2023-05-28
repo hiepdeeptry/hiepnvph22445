6a5125ac6709c231bd5dea4a354cb1eac5b076c7
hiepdeeptry <133551147+hiepdeeptry@users.noreply.github.com>
2023-05-28 07:20:54 +0700

    update

--- HelloWorld/screens/SignInScreen.js
+++ HelloWorld/screens/SignInScreen.js
@@ -11,6 +11,28 @@ const SignInScreen = ({ navigation }) => {
 
   let users = [];
 
+  async function fetchData() {
+    try {
+      const response = await fetch('http://192.168.2.104:3000/users');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log('Lỗi lấy dữ liệu! ' + error);
+      return null;
+    }
+  }
+
+  async function getData() {
+    users = await fetchData();
+  }
+
+  getData();
+
+  const navigateHome = () => {
+    navigation.navigate('Home')
+
+  };
+
 
   const onPressSignIn = () => {
     // let request = { username: username, password: password };
@@ -34,49 +56,28 @@ const SignInScreen = ({ navigation }) => {
     let request = { username: username, password: password };
 
     if (users) {
+      console.log(users);
       const authInfo = users.find((user) => user.userName === request.username);
 
       if (!authInfo) {
-          Alert.alert('Thông báo!', 'Không tìm thấy tài khoản', [{ text: 'Cancel', onPress: () => console.log('Không tìm thấy user:  ' + request.username) }]);
+        Alert.alert('Thông báo!', 'Không tìm thấy tài khoản', [{ text: 'Cancel', onPress: () => console.log('Không tìm thấy user:  ' + request.username) }]);
       } else {
-          if (!(authInfo.password === request.password)) {
-              Alert.alert('Thông báo', 'Mật khẩu không chính xác!', [{ text: 'Cancel', onPress: () => console.log('Mật khẩu sai với user:  ' + request.username) }]);
-          } else {
-              Alert.alert('Thông báo', 'Đăng nhập thành công! ' + request.username, [
-                  { text: 'OK', onPress: () => navigateToHome() },
-                  { text: 'Cancel', onPress: () => console.log('Press Cancel') }
-              ]);
-          }
+        if (!(authInfo.password === request.password)) {
+          Alert.alert('Thông báo', 'Mật khẩu không chính xác!', [{ text: 'Cancel', onPress: () => console.log('Mật khẩu sai với user:  ' + request.username) }]);
+        } else {
+          Alert.alert('Thông báo', 'Đăng nhập thành công! ' + request.username, [
+            { text: 'OK', onPress: () => navigateHome() },
+            { text: 'Cancel', onPress: () => console.log('Press Cancel') }
+          ]);
+        }
       }
-  }
-
-
-
-  };
-
-
-
-  async function fetchData() {
-    try {
-      const response = await fetch('http://localhost:3000/users');
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.log('Lỗi lấy dữ liệu! ' + error);
-      return null;
     }
-  }
-
-  async function getData() {
-    users = await fetchData();
-  }
-
- getData();
-
-  const navigateHome = () => {
-    navigation.navigate('Home')
 
   };
+
+
+
+
 
   return (
     <View style={styles.container}>
